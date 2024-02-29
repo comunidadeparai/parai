d42dfc5d93b015aad0ecb46e0431c270d64f8459
Miquéias Soares <mqseraos@gmail.com>
2024-02-29 09:25:12 -0300

    Create add new "ecossistema" json

---
+++ js/ecossistemas.js
@@ -0,0 +1,20 @@
+const url = "data/ecossistemas.json";
+const divEcossistemas = document.querySelector(".ecossistemas");
+
+fetch(url).then((res) => {
+    res.json().then((dados) => {
+        const ecossistemas = dados.ecossistemas;
+        console.log(ecossistemas)
+        ecossistemas.forEach((item) => {
+        
+            const template = `<tr>
+                                <td><a href="${item.linkSite}" target="_blank">${item.txtSite}</a></td>
+                                <td>${item.nome} - ${item.tipo}</td>
+                            </tr>`
+
+            
+            divEcossistemas.innerHTML += template;
+            
+        });
+    })    
+})
